1aef2773f14b58f4ecd7fc7c06840f04c5d104e8
Bhuvaneswar562000 <bhuvaneshwarnaidu26@gmail.com>
2024-11-14 13:48:55 +0530

    Metrics added

---
+++ Server/src/models/metric.model.js
@@ -0,0 +1,29 @@
+
+import mongoose from "mongoose";
+
+const metricSchema = new mongoose.Schema({
+    equipmentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Equipment',
+        required: true
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ['temperature', 'cpuLoad', 'memoryUsage', 'powerUsage', 
+                'fanSpeed', 'humidity', 'airflow',]
+    },
+    value: {
+        type: Number,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+}, { timestamps: true });
+
+// Index for quick queries
+metricSchema.index({ equipmentId: 1, type: 1, timestamp: -1 });
+
+export default mongoose.model('Metric', metricSchema);
