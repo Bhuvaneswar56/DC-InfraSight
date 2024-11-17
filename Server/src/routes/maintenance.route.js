2898c3d6b55a5f75022f36acb509520b9517de95
irfan <irfan.i.ahmed@gmail.com>
2024-11-17 12:17:53 +0530

    Add: maintenance schedule api

--- Server/src/routes/maintenance.route.js
+++ Server/src/routes/maintenance.route.js
@@ -1,7 +1,13 @@
 // routes/equipment.route.js
 import express from 'express';
 import {
-    createMaintenance
+    createMaintenance,
+    cancelMaintenance,
+    rescheduleMaintenance,
+    updateMaintenanceStatus,
+    getMaintenanceByEquipId,
+    getMaintenanceById,
+    getAllMaintenance
 } from '../controllers/maintenance.controller.js';
 import { verifyToken, authorize } from '../middlewares/verifyToken.middleware.js';
 
@@ -18,5 +24,33 @@ router.post('/',
     createMaintenance
 );
 
+router.delete('/:maintenanceId',
+    verifyToken,
+    cancelMaintenance
+);
 
+router.post('/:maintenanceId',
+    verifyToken,
+    rescheduleMaintenance
+);
+
+router.post('/:maintenanceId',
+    verifyToken,
+    updateMaintenanceStatus
+);
+
+router.get('/:equipId',
+    verifyToken,
+    getMaintenanceByEquipId
+);
+
+router.get('/:maintenanceId',
+    verifyToken,
+    getMaintenanceById
+);
+
+router.get('/',
+    verifyToken,
+    getAllMaintenance
+);
 export default router;
