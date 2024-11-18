201616a7f5ed1bb17914aba9030dfee9862c9aad
irfan <irfan.i.ahmed@gmail.com>
2024-11-18 08:21:13 +0530

    Debugging alert controller

--- Server/src/controllers/alert.controller.js
+++ Server/src/controllers/alert.controller.js
@@ -1,41 +1,33 @@
 // controllers/alert.controller.js
-import Alert from '../models/alert.model.js';
+import alertModel from '../models/alert.model.js';
+import ApiResponse from '../utils/ApiResponse.js';
+import { ApiError } from '../utils/ApiError.js';
+import asyncHandler from '../utils/asyncHandler.js'
 
-export const getAlerts = async (req, res, next) => {
-    try {
-        const alerts = await Alert.find()
-            .sort({ timestamp: -1 })
-            .populate('metrics_id');
-            
-        res.status(200).json({
-            success: true,
-            data: alerts
-        });
-    } catch (error) {
-        next(error);
+const getAlerts = asyncHandler(async (req, res) => {
+
+    const alerts = await alertModel.find()
+        .sort({ timestamp: -1 })
+        .populate('metrics_id');
+
+    res.status(200).json(new ApiResponse(200, "Alerts fetch successful", alerts));
+});
+
+const updateAlertStatus = asyncHandler(async (req, res) => {
+
+    const { status } = req.body;
+    const alert = await alertModel.findByIdAndUpdate(req.params.id, { status }, { new: true });
+
+    if (!alert) {
+        throw new ApiError(404, "Alert not found");
     }
-};
 
-export const updateAlertStatus = async (req, res, next) => {
-    try {
-        const { status } = req.body;
-        const alert = await Alert.findByIdAndUpdate(
-            req.params.id,
-            { status },
-            { new: true }
-        );
-
-        if (!alert) {
-            return next(createError(404, "Alert not found"));
-        }
-
-        res.status(200).json({
-            success: true,
-            data: alert
-        });
-    } catch (error) {
-        next(error);
-    }
-};
+    res.status(200).json(new ApiResponse(200, "Update alert successful", alert));
+});
 
 // Add other alert-related controllers
+
+export {
+    getAlerts,
+    updateAlertStatus
+};
--- Server/src/controllers/location.controller.js
+++ Server/src/controllers/location.controller.js
@@ -4,19 +4,19 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import userModel from "../models/user.model.js";
 
-const createLocation = asyncHandler(async(req,res)=>{
+const createLocation = asyncHandler(async (req, res) => {
     const {
         name,
         type,
         floor,
         building,
-        capacity:{power, cooling, totalRacks},
+        capacity: { power, cooling, totalRacks },
         status,
     } = req.body;
-   
+
     const existingLocation = await locationModel.findOne({ name, type, floor });
 
-    if(existingLocation){
+    if (existingLocation) {
         throw new ApiError(400, "Location already exists with the same name, type, and floor.");
     }
 
@@ -24,12 +24,12 @@ const createLocation = asyncHandler(async(req,res)=>{
     //     throw new ApiError(400, "Capacity must include power, cooling, and racks subfields.");
     // }
 
-    let locationData={
+    let locationData = {
         name,
         type,
         floor,
         building,
-        capacity:{power, cooling, totalRacks},
+        capacity: { power, cooling, totalRacks },
         status
     }
 
@@ -38,33 +38,28 @@ const createLocation = asyncHandler(async(req,res)=>{
     res
         .status(200)
         .json(new ApiResponse(200, "Location Created", location));
-
 })
 
 
-const getAllLocations = asyncHandler(async(req,res)=>{
-   
+const getAllLocations = asyncHandler(async (req, res) => {
+
     let allLocations = await locationModel.find()
     res
         .status(200)
         .json(new ApiResponse(200, "Location Created", allLocations));
-
-
 })
 
-const getLocationById = asyncHandler(async(req,res)=>{
-    const {id} = req.params
-    
-    let locationById = await locationModel.findById(id)
+const getLocationById = asyncHandler(async (req, res) => {
+    const { id } = req.params
+
+    let locationById = await locationModel.findById(id);
     res
         .status(200)
         .json(new ApiResponse(200, "Location Created", locationById));
-
-
 })
 
 const updateLocation = asyncHandler(async (req, res) => {
-    const { id } = req.params; 
+    const { id } = req.params;
     const {
         name,
         type,
@@ -72,7 +67,7 @@ const updateLocation = asyncHandler(async (req, res) => {
         building,
         capacity: { power, cooling, totalRacks },
         status,
-    } = req.body; 
+    } = req.body;
 
     const existingLocation = await locationModel.findById(id);
     if (!existingLocation) {
@@ -82,17 +77,17 @@ const updateLocation = asyncHandler(async (req, res) => {
     const updatedLocation = await locationModel.findByIdAndUpdate(
         id,
         { name, type, floor, building, capacity: { power, cooling, totalRacks }, status },
-        {new:true}
+        { new: true }
     );
 
     res
-    .status(200)
-    .json(new ApiResponse(200, "Location Updated", updatedLocation));
+        .status(200)
+        .json(new ApiResponse(200, "Location Updated", updatedLocation));
 });
 
 const deleteLocation = asyncHandler(async (req, res) => {
-    const { id } = req.params; 
-   
+    const { id } = req.params;
+
     const existingLocation = await locationModel.findById(id);
     if (!existingLocation) {
         throw new ApiError(404, "Location not found");
@@ -101,8 +96,14 @@ const deleteLocation = asyncHandler(async (req, res) => {
     await locationModel.findByIdAndDelete(id);
 
     res
-    .status(200)
-    .json(new ApiResponse(200, "Location Deleted", { id }));
+        .status(200)
+        .json(new ApiResponse(200, "Location Deleted", { id }));
 });
 
-export { createLocation, getAllLocations, getLocationById,updateLocation, deleteLocation };
+export {
+    createLocation,
+    getAllLocations,
+    getLocationById,
+    updateLocation,
+    deleteLocation
+};
